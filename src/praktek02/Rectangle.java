5041914c9b8f29c682f440a65faf92ef2833015f
Ahmadrizal18 <rizalvantasy@gmail.com>
2022-12-17 09:45:45 +0800

    Memanggil Method Behavior

--- src/praktek02/Rectangle.java
+++ src/praktek02/Rectangle.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek02;
 public class Rectangle {
     double panjang;
     double lebar;
     
-    cetakInfo(){
+    void cetakInfo(){
         System.out.println("==================");
         System.out.println("Panjang : "+panjang);
         System.out.println("Lebar :   "+lebar);
--- src/praktek02/RectangleAksi.java
+++ src/praktek02/RectangleAksi.java
@@ -2,9 +2,11 @@ package praktek02;
 
 public class RectangleAksi {
     public static void main(String[] args) {
-        Rectangle rl = new Rectangle();
+        Rectangle r1 = new Rectangle();
         r1.panjang =9;
         r1.lebar =3;
+        
+        r1.cetakInfo();
     }
     
 }
